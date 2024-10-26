65172ed8c164b659dc112e1250a7728b9088686e
JackieWang <kaijunwangbetter@gmail.com>
2024-10-26 20:50:26 +0800

    First commit

---
+++ src/components/about.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-start leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        Now I'm a <span className="font-medium">research assistant</span>
+        in AI4SG Lab at School of Computing, National University of Singapore.
+        I'm working with <span className="font-medium">Dr. Yi-Chieh Lee</span>.
+        Before that, I earned my bachelor's degree in Computer Science and
+        Technology from Shanghai Jiao Tong University in 2024. During my
+        undergraduate studies, I'm fortunate to have the opportunity to work
+        with <span className="font-medium">Dr. Yanyan Shen</span> and{" "}
+        <span className="font-medium">Dr. Bingsheng He</span> on several
+        research.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy watching
+        videos, and listening to music. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about <span className="font-medium">Music and Japanese</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ src/components/intro.tsx
@@ -0,0 +1,92 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import sketch from "@/../public/personal sketch.png";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
+export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              delay: 0.1,
+              duration: 0.3,
+            }}
+          >
+            <Image
+              src={sketch}
+              alt="Personal protrait"
+              width={200}
+              height={200}
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-slate-500 shadow-xl"
+            />
+          </motion.div>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-xl font-medium !leading-[1.5] sm:text-2xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Kaijun Wang.</span> I'm a{" "}
+        <span className="font-bold">researcher</span> at School of Computing,
+        National University, focusing on human-computer interaction (
+        <span className="font-bold">HCI</span>).
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/resume.pdf"
+          target="_blank"
+          //   download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
